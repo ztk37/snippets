38553bdf352d09bb4b24958b62f7feb007e5d979
Zetrik <ztk37.contact@gmail.com>
2021-03-06 13:15:28 +0100

    Update multiline_variable.sh

--- bash/multiline_variable.sh
+++ bash/multiline_variable.sh
@@ -17,3 +17,21 @@ read -r -d '' DATA <<EOD
 EOD
 
 echo $DATA;
+
+read -r -d '' DATA <<EOD
+{
+  "token": "sdad9asd7as8d7as7d676as766ef76sf"
+}
+EOD
+
+echo $DATA;
+
+read -d '' RAW <<EOD
+{
+ "token": "asdasd9a)=Sd=87a8c78sC"
+}
+EOD
+
+echo $RAW;
+
+curl -q -XGET "https://swapi.dev/api/people/1"
