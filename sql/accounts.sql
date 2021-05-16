c99b9aca8d4bc4b4bc59cd3287597f0d91634912
Zetrik <30878725+ztk37@users.noreply.github.com>
2021-05-16 18:38:04 +0200

    Update accounts.sql

--- sql/accounts.sql
+++ sql/accounts.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS `accounts` (
-  `username` VARCHAR(255) NOT NULL,
-  `password` VARCHAR(255) NOT NULL
-); 
+CREATE TABLE IF NOT EXISTS `accounts`
+  ( `username` VARCHAR(255) NOT NULL
+  , `password` VARCHAR(255) NOT NULL
+  ); 
