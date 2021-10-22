011356ce0f6ab4159d6be06a08280c51f3bc5543
Zetrik <30878725+ztk37@users.noreply.github.com>
2021-10-22 19:45:26 +0200

    Update Free.hs

--- haskell/Free.hs
+++ haskell/Free.hs
@@ -1,4 +1,6 @@
-{-# Language DeriveFunctor #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Main (main) where
 
@@ -17,44 +19,84 @@ data Free f a
 
 instance Functor f => Functor (Free f) where
   fmap f (Pure a) = Pure (f a)
-  fmap f (Free x) = Free (fmap (fmap f) x)
-  -- fmap g (Free fx) = Free (fmap g <$> fx)
+  fmap f (Free x) = Free (fmap f <$> x)
 
 instance Functor f => Applicative (Free f) where
   pure = Pure
-  (Pure f) <*> (Pure x) = pure $ f x
-  (Pure f) <*> (Free wwx) = Free ((f <$>) <$> wwx)
-  (Free wwf) <*> ax = Free ((<*> ax) <$> wwf)
+  Pure f <*> Pure a = Pure (f a)
+  Pure f <*> Free x = Free (fmap f <$> x)
+  Free x <*> my     = Free ((<*> my) <$> x)
 
 instance Functor f => Monad (Free f) where
-  return = Pure
-  x >>= f = concatFree (fmap f x)
-
-liftFree :: Functor f => f a -> Free f a
-liftFree x = Free (fmap Pure x)
-
-foldFree :: Functor f => (f r -> r) -> Free f r -> r
-foldFree _ (Pure a) = a
-foldFree f (Free x) = f (fmap (foldFree f) x)
-
-concatFree :: Functor f => Free f (Free f a) -> Free f a
-concatFree (Pure x) = x
-concatFree (Free y) = Free (fmap concatFree y)
+  return = pure
+  Pure a >>= f = f a
+  Free x >>= f = Free ((>>= f) <$> x)
 
 data EffectF next
-  = EffEcho String next
-  -- deriving (Functor)
+  = EchoEff String next
+  | PromptEff (String -> next)
+  deriving Functor
 
--- instance Functor EffectF where
---  fmap g (EffEcho s next) = _ -- EffEcho s (g . next) 
+type Effect next = Free EffectF next
 
-instance Functor EffectF where
-  fmap g (EffEcho s next) = EffEcho s (g next)
+liftF :: Functor f => f a -> Free f a
+liftF = Free . fmap return
 
+foldF
+    :: (Functor f, Monad m)
+    => (forall a. f a -> m a)
+    -> Free f a
+    -> m a
+foldF morph (Pure a) = return a
+foldF morph (Free f) = do
+    a <- morph f
+    foldF morph a
+   
+concatF :: Functor f => Free f (Free f a) -> Free f a
+concatF (Pure x) = x
+concatF (Free y) = Free (fmap concatF y)
 
-type Effect = Free EffectF
+echo :: String -> Effect ()
+echo s = liftF $ EchoEff s ()
 
-echo s = liftFree $ EffEcho s
+prompt :: Effect String
+prompt = Free $ PromptEff return
+
+programm :: Effect ()
+programm = do
+  s <- prompt
+  echo "Foo"
+  echo s
+
+interpret :: Effect a -> IO a
+interpret = foldF morph
+  where
+    morph :: EffectF a -> IO a
+    morph (EchoEff s next) = do
+      putStrLn s
+      pure next
+    morph (PromptEff next) = do
+      s <- getLine
+      pure $ next s
+
+interpret' :: Effect a -> IO a
+interpret' = foldF $ \case 
+    (EchoEff s next) -> do
+      putStrLn s
+      pure next
+    (PromptEff next) -> do
+      s <- getLine
+      pure $ next s
+
+interpret'' :: Effect a -> IO a
+interpret'' = foldF $ \case 
+    (EchoEff s next) -> 
+      next <$ putStrLn s
+    (PromptEff next) ->
+      next <$> getLine
 
 main :: IO ()
-main = putStrLn "Hello, World!"
+main = do
+  interpret programm
+  interpret' programm
+  interpret'' programm
