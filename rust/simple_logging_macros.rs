efee790043cd7c980780b7fcd21093e9141a2caa
Zetrik <30878725+ztk37@users.noreply.github.com>
2022-10-06 21:16:23 +0200

    Update simple_logging_macros.rs

--- rust/simple_logging_macros.rs
+++ rust/simple_logging_macros.rs
@@ -19,9 +19,28 @@ macro_rules! err {
     }
 }
 
+#[macro_export]
+macro_rules! log {
+    (red, $arg:expr, $($rest:tt)*) => {
+        println!("{} {}", ansi_term::Color::Red.paint($arg), $($rest)*);
+    };
+
+    (green, $arg:expr, $($rest:tt)*) => {
+        println!("{} {}", ansi_term::Color::Green.paint($arg), $($rest)*);
+    };
+
+    (blue, $arg:expr, $($rest:tt)*) => {
+        println!("{} {}", ansi_term::Color::Blue.paint($arg), $($rest)*);
+    };
+}
 
 fn main() {
     info!("some message");
     warn!("some message");
     err!("some message");
+    
+    
+    log!(red, "* something in red", "some message");
+    log!(green, "* something in green", "some message");
+    log!(blue, "* something in blue", "some message");
 }
