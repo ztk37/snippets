312069eb5d6f3c12419fd09ed10347e5c076e0b8
Zetrik <30878725+ztk37@users.noreply.github.com>
2023-07-26 21:20:15 +0200

    Update and rename shuffle.js to shuffle.ts

---
+++ typescript/shuffle.ts
@@ -0,0 +1,16 @@
+const xs = [1, 2, 3, 4, 5, 6, 7]
+
+function shuffle<T>(xs: T[]): T[] {
+    let shuffled = xs.slice()
+
+    for (let i = shuffled.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]
+    }
+
+    return shuffled
+}
+
+console.log("before:", xs)
+console.log("shuffled:", shuffle(xs))
+console.log("after:", xs)
